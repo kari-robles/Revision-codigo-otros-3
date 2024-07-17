e4de679188f73639de376b16c5bbf9c1df4ef55c
Karina Robles <roblesks92@gmail.com>
2024-07-17 00:41:34 -0600

    first commit

--- filtro.js
+++ filtro.js
@@ -6,58 +6,69 @@ const productos = [
   {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
   {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+];
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+ const li = document.getElementById('lista-de-productos');//getElementbyId
+ const i = document.querySelector('input'); //i quite $ para definir la variable i
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
-
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+ const displayProductos = (productos) => { //funcion display productos
+ for (let i = 0; i < productos.length; i++) {
   
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+    var div = document.createElement('div') // me fui a html y puse div
+    div.classList.add('producto') // puse la clase en html
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+    var ti = document.createElement('ti') 
+    ti.classList.add('titulo')
+    ti.textContent = productos[i].nombre
+  
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
 
-  li.appendChild(d)
+    div.appendChild(ti) // cambie a div
+    div.appendChild(imagen) // cambie a div
+  
+    li.appendChild(div) // cambie a div
+ }
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+displayProductos(productos);
+ 
 
+
+const botonDeFiltro = document.querySelector('button');
+ 
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
+  
+const texto = i.value; //es i
+console.log(texto);
 
-  const texto = $i.value;
-  console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+
+  const productosFiltrados = filtrado(productos, texto ); //definimos la funcion productosFiltrados para usarla abajo
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+    var div = document.createElement("div")
+    div.classList.add("producto")
   
-    var ti = document.createElement("p")
+    var ti = document.createElement("ti")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
     
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    div.appendChild(ti) //aqui cambie variables porque me acomode un poco mas 
+    div.appendChild(imagen)
   
-    li.appendChild(d)
+    li.appendChild(div)
   }
 }
 
 const filtrado = (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }  
+
+
+
